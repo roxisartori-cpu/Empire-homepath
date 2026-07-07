3bb6787840a267075c7787880208eca7fa372952
roxisartori-cpu <roxisartori@gmail.com>
2026-07-07 16:15:12 -0400

    Update HomePage.jsx

--- src/pages/static/HomePage.jsx
+++ src/pages/static/HomePage.jsx
@@ -4,9 +4,15 @@ export default function HomePage() {
       src="/empirehomepath-v8.html"
       style={{
         width: '100%',
-        height: '100vh',
+        height: '100%',
+        minHeight: '100vh',
         border: 'none',
         display: 'block',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
       }}
       title="Empire HomePath"
     />
